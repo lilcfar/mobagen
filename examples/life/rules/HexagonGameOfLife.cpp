1eb00a867bc4bd4854259a3696f4c6477ba5645e
lilcfar <lilyfarr.daisy@gmail.com>
2025-09-12 15:19:48 -0400

    implementing games of life

--- examples/life/rules/HexagonGameOfLife.cpp
+++ examples/life/rules/HexagonGameOfLife.cpp
@@ -3,5 +3,38 @@
 //
 
 #include "HexagonGameOfLife.h"
-void HexagonGameOfLife::Step(World& world) {}
-int HexagonGameOfLife::CountNeighbors(World& world, Point2D point) { return 0; }
+void HexagonGameOfLife::Step(World& world) {
+  const int wrldSz = world.SideSize(); //nxn
+
+  for (int y = 0; y < wrldSz; ++y) {
+    for (int x = 0; x < wrldSz; ++x) {
+      const bool alive = world.Get({x, y});
+      const int  n = CountNeighbors(world, {x, y});
+
+      const bool nextAlive = alive ? (n == 2 || n == 3) : (n == 3);
+
+      world.SetNext({x, y}, nextAlive);
+    }
+  }
+
+  world.SwapBuffers();
+}
+
+int HexagonGameOfLife::CountNeighbors(World& world, Point2D point) {
+  int count = 0;
+  const int p = (point.y & 1);
+
+  // beside
+  if (world.Get({point.x - 1, point.y})) ++count;
+  if (world.Get({point.x + 1, point.y})) ++count;
+
+  //abouve
+  if (world.Get({point.x + p - 1, point.y - 1})) ++count;
+  if (world.Get({point.x + p,     point.y - 1})) ++count;
+
+  // below
+  if (world.Get({point.x + p - 1, point.y + 1})) ++count;
+  if (world.Get({point.x + p,     point.y + 1})) ++count;
+
+  return count;
+}
--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -3,9 +3,31 @@
 // Reference: https://playgameoflife.com/info
 void JohnConway::Step(World& world) {
   // todo: implement
+  const int wrldSz = world.SideSize(); //nxn
+
+  for (int y = 0; y < wrldSz; ++y) {
+    for (int x = 0; x < wrldSz; ++x) {
+      const bool alive = world.Get({x, y});
+      const int  n = CountNeighbors(world, {x, y});
+      const bool nextAlive = alive ? (n == 2 || n == 3) : (n == 3);
+
+      world.SetNext({x, y}, nextAlive);
+    }
+  }
+
+  world.SwapBuffers();
 }
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
   // todo: implement
-  return 0;
+  int count = 0;
+  for (int i = -1; i <= 1; ++i)
+  {
+    for (int j = -1; j <= 1; ++j)
+    {
+      if (i == 0 && j == 0) continue;
+      if (world.Get({point.x + j, point.y +i})) ++count;
+    }
+  }
+  return count;
 }
